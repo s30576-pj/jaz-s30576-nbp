ece3c6e9f629be0b8e09c3d5a51c0502b183807c
Natan Brzozowski <s30576@pjwstk.edu.pl>
2025-06-21 15:16:27 +0200

    [NBP] first commit

---
+++ src/main/java/pl/pjatk/jaz_s30576_nbp/JazS30576NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz_s30576_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS30576NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS30576NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/jaz_s30576_nbp/JazS30576NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz_s30576_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS30576NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
